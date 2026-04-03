267b1e682c5990497ee392678fab2eeef8fb64f0
Cristian Blanco <cris0291@gmail.com>
2026-04-02 21:41:39 -0600

    add viper and start processing commands

--- cmd/mini/main.go
+++ cmd/mini/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	run := pflag.NewFlagSet("run", pflag.ContinueOnError)
+
+	cmd := run.StringP("cmd", r, "/bin/sh", "command to run")
+	rootfs := run.StringP("rootfs", "f", "/rootfs", "path to root")
+
+	run.Parse()
+	viper.BindPFlags(run)
 }
