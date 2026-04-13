c662743f7cf943b745faef7abf02e284ab35e1c0
Cristian Blanco <cris0291@gmail.com>
2026-04-12 20:45:11 -0600

    Start implementing main fucntion as a life cycle orchestrator

--- cmd/mini/main.go
+++ cmd/mini/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
-	run := pflag.NewFlagSet("run", pflag.ContinueOnError)
-
-	cmd := run.StringP("cmd", r, "/bin/sh", "command to run")
-	rootfs := run.StringP("rootfs", "f", "/rootfs", "path to root")
-
-	run.Parse()
-	viper.BindPFlags(run)
+	lifeCycleCommand := os.Args[1:][0]
+	switch lifeCycleCommand {
+	case "create":
+		create()
+	case "run":
+		run()
+	case "start":
+		start()
+	case "init":
+		runInit()
+	}
 }
