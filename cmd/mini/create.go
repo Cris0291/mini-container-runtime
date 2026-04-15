3e64be210e4478498ed305bcbc9836cdf41813fb
Cristian Blanco <cris0291@gmail.com>
2026-04-14 20:51:30 -0600

    Add resource and networking structs

--- cmd/mini/create.go
+++ cmd/mini/create.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 type ContainerConfig struct {
@@ -19,7 +20,43 @@ type ContainerConfig struct {
 	Networking *NetworkingConfig
 }
 
-type ProcessConfig struct{}
+type ProcessConfig struct {
+	Args []string
+	Env  []string
+	Cwd  string
+	UID  string
+	GID  string
+}
+
+type Mount struct {
+	Source      string
+	Destination string
+	Type        string
+	Flags       int
+	Data        string
+}
+
+type Namespace struct {
+	Type string
+	Path string
+}
+
+type ResourceConfig struct {
+	MemoryLimit int64
+	CpuShares   int64
+	PidsLimit   int64
+}
+
+type NetworkingConfig struct {
+	IP      string
+	GateWay string
+	Bridge  string
+}
 
 func create(jsonConfig string) {
+	jsonConfigSlice := []byte(jsonConfig)
+	err := json.Unmarshal(jsonConfigSlice, &ContainerConfig)
+	if err != nil {
+		fmt("error")
+	}
 }
