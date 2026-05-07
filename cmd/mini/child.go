8f03c687300a53caecd81691c0dda62778c9006e
Cristian Blanco <cris0291@gmail.com>
2026-05-06 20:52:56 -0600

    keep implementing child

--- cmd/mini/child.go
+++ cmd/mini/child.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"os"
+	"strconv"
+	"syscall"
 )
 
-func child_init() {
-	fd := os.Getenv("_MYCONTAINER_CONFIGPIPE")
+func child_init() error {
+	envFileDescriptor := os.Getenv("_MYCONTAINER_CONFIGPIPE")
+	fd, err := strconv.Atoi(envFileDescriptor)
+	if err != nil {
+		return err
+	}
+
+	file := os.NewFile(uintptr(fd), "config-pipe")
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+
+	var config ContainerConfig
+
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		return nil
+	}
+
+	err = syscall.Sethostname([]byte("container"))
+	if err != nil {
+		return err
+	}
 }
