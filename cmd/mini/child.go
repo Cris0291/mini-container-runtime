c9b4461895b7eed981fb75a2df9949106728762b
Cristian Blanco <cris0291@gmail.com>
2026-05-09 21:57:37 -0600

    Add mount virtual file systems inside rootfs

--- cmd/mini/child.go
+++ cmd/mini/child.go
@@ -4,11 +4,27 @@ import (
 	"encoding/json"
 	"io"
 	"os"
+	"path/filepath"
 	"strconv"
 	"syscall"
 )
 
-func child_init() error {
+func MountVirtualFileSystems(config *ContainerConfig) error {
+	for _, mount := range config.Mounts {
+		path := filepath.Join(config.Rootfs, mount.Destination)
+		err := os.Mkdir(path, 0o711)
+		if err != nil {
+			return err
+		}
+		err = syscall.Mount(mount.Source, path, mount.Source, uintptr(mount.Flags), mount.Data)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ChildInit() error {
 	envFileDescriptor := os.Getenv("_MYCONTAINER_CONFIGPIPE")
 	fd, err := strconv.Atoi(envFileDescriptor)
 	if err != nil {
@@ -19,6 +35,9 @@ func child_init() error {
 	defer file.Close()
 
 	content, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
 
 	var config ContainerConfig
 
@@ -27,8 +46,16 @@ func child_init() error {
 		return nil
 	}
 
-	err = syscall.Sethostname([]byte("container"))
+	err = syscall.Sethostname([]byte(config.Hostname))
 	if err != nil {
 		return err
 	}
+
+	// Mount all virtuall filesystems
+	err = MountVirtualFileSystems(&config)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
