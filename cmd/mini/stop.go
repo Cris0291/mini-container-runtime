175f328ed2a1b3f081752d5b33452e1fbaf12b30
Cristian Blanco <cris0291@gmail.com>
2026-06-20 21:36:48 -0600

    SIGTERM and SIGKILL process termination

--- cmd/mini/stop.go
+++ cmd/mini/stop.go
@@ -6,7 +6,40 @@ import (
 	"path/filepath"
 	"strconv"
 	"syscall"
+	"time"
 )
+
+func writeStopState(state *ContainerState, statePath *string) error {
+	state.Status = "stopped"
+	data, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(*statePath, data, 0o644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func terminateProcess(pid int, timeout time.Duration, state *ContainerState, statePath *string) error {
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		// Process was killed in between just save the stopped state
+		err = writeStopState(state, statePath)
+		return err
+	}
+
+	err = p.Signal(syscall.SIGTERM)
+	if err != nil {
+		// Process was already kill
+		writeStopState(state, statePath)
+	}
+
+	deadline := time.Now().Add(timeout)
+	for time.Now().Before(deadline) {
+	}
+}
 
 func stop(containerID string) error {
 	containerPath := filepath.Join("/run/mycontainer", containerID)
@@ -37,15 +70,7 @@ func stop(containerID string) error {
 	childPID := strconv.Itoa(state.PID)
 	_, err = os.Stat("/proc/" + childPID)
 	if err != nil {
-		state.Status = "stopped"
-		data, err := json.Marshal(state)
-		if err != nil {
-			return err
-		}
-		err = os.WriteFile(statePath, data, 0o644)
-		if err != nil {
-			return err
-		}
-		return nil
+		err = WriteStopState(&state, &statePath)
+		return err
 	}
 }
