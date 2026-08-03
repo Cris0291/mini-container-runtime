b051aa5a0788a25c44deb78d94f78fbd781ce80e
Cristian Blanco <cris0291@gmail.com>
2026-08-02 21:01:24 -0600

    start adding cgrup kill in stop

--- cmd/mini/stop.go
+++ cmd/mini/stop.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"internal/runtime/cgroup"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -23,7 +25,46 @@ func writeStopState(state *ContainerState, statePath *string) error {
 	return nil
 }
 
-func terminateProcess(pid int, timeout time.Duration) error {
+func readCgroupPids(cgroupPath *string) ([]int, error) {
+	data, err := os.ReadFile(*cgroupPath)
+	if err != nil {
+		return nil, err
+	}
+
+	str := string(data)
+
+	var pids []int
+	for _, line := range strings.Fields(str) {
+		pid, err := strconv.Atoi(line)
+		if err != nil {
+			continue
+		}
+
+		pids = append(pids, pid)
+	}
+
+	return pids, nil
+}
+
+func signalCgroups(cgroupPath *string, signal syscall.Signal) error {
+	pids, err := readCgroupPids(cgroupPath * string)
+	if err != nil {
+		return err
+	}
+
+	for _, pid := range pids {
+		err = syscall.Kill(pid, signal)
+	}
+}
+
+func terminateProcess(cgroupPath *string, timeout time.Duration) error {
+	pids, err := readCgroupPids(cgroupPath)
+	if err != nil {
+		return err
+	}
+
+	for _, pid := range pids {
+	}
 	p, err := os.FindProcess(pid)
 
 	err = p.Signal(syscall.SIGTERM)
@@ -40,13 +81,18 @@ func terminateProcess(pid int, timeout time.Duration) error {
 		time.Sleep(100 * time.Millisecond)
 	}
 
-	return p.Kill()
+	// instead of p.kill we use cgroups.kill in order to guarantee
+	// that all the processes
+	killFile := filepath.Join(*cgroupPath, "cgroup.kill")
+	err = os.WriteFile(killFile, []byte("1"), 0o200)
+	return err
 }
 
 func stop(containerID string) error {
 	containerPath := filepath.Join("/run/mycontainer", containerID)
 	statePath := filepath.Join(containerPath, "state.json")
 	lockPath := filepath.Join(containerPath, "lock")
+	cgroupPath := "/sys/fs/cgroup/mycontainer"
 
 	fileLock, err := os.OpenFile(lockPath, syscall.O_RDWR, 0)
 	if err != nil {
