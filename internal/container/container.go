1d4d90eb803dab59cfe518434436be7205e06b16
Cristian Blanco <cris0291@gmail.com>
2026-09-08 20:26:47 -0600

    add init for container

--- internal/container/container.go
+++ internal/container/container.go
@@ -1,7 +1,28 @@
 package main
 
-struct Container {
-	
+import "path/filepath"
+
+type Container struct {
+	ContainerDirPath   string
+	ContainerPath      string
+	ContainerID        string
+	ContainerStatePath string
+	ContainerLockPath  string
+	ContainerFifoPath  string
+	statePath          string
+	lockPath           string
+	fifoPath           string
 }
 
+func NewContainer(containerDir string, containerID string, state string, lock string, fifo string) *Container {
+	c := &Container{ContainerDirPath: containerDir, ContainerID: containerID, statePath: state, lockPath: lock, fifoPath: fifo}
+	c.initialize()
+	return c
+}
 
+func (container *Container) initialize() {
+	container.ContainerPath = filepath.Join(container.ContainerDirPath, container.ContainerID)
+	container.ContainerStatePath = filepath.Join(container.ContainerPath, container.statePath)
+	container.ContainerLockPath = filepath.Join(container.ContainerPath, container.lockPath)
+	container.ContainerFifoPath = filepath.Join(container.ContainerPath, container.fifoPath)
+}
